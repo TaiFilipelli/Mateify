388292e7f882b2ececde9c1a488eb0c68ef5d502
TaiFilipelli <taielfilipelli@gmail.com>
2024-09-20 11:22:05 -0300

    primeras fonts: roboto para encabezados y montserrat para contenidos.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   darkMode:'class',
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      'fontFamily':{
+        'mont-light':'Mont Light',
+        'mont-light-italic':'Mont Light Italic',
+        'mont-md':'Mont Medium',
+        'mont-md-italic':'Mont Medium Italic',
+        'mont-thin':'Mont Thin',
+        'mont-thin-italic':'Mont Thin Italic',
+        'robtop-black':'Roboto Black',
+        'robtop-bold':'Roboto Bold',
+        'robtop-bold-italic':'Roboto Bold Italic'
+      }
+    },
   },
   plugins: [],
 }

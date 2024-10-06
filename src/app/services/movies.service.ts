ee090abc3b036d82ee08f22fa7b647939062d1ff
TaiFilipelli <taielfilipelli@gmail.com>
2024-10-06 19:02:08 -0300

    algunos ajustes con los datos, la key innecesaria en la url al añadirse desde headers y algunas lineas extra en el componente de pelicula

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -12,15 +12,15 @@ export class MoviesService {
 
   //KEY DE LECTURA
   private API_KEY_READ = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjFiZjEzYTI1ZTI3NjFjOWU0ZDAxNDNlM2QxZDA5MiIsIm5iZiI6MTcyODI0OTA3OC40MTM5NzUsInN1YiI6IjY3MDJmYzVhZjg3OGFkZmVkMDg1Yjk0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mI_o9KbsorkK_XthOUfBDQgHCqdd5gTzRrlL5HjJQVU'
-  private baseUrl:string = 'https://api.themoviedb.org/3/movie/popular?api_key=ff1bf13a25e2761c9e4d0143e3d1d092&language=es-ES&page=1';
+  private baseUrl:string = 'https://api.themoviedb.org/3/movie/popular?language=es-ES&page=1';
 
   constructor(private http: HttpClient) { }
 
     getMovies() {
       const headers = new HttpHeaders({
         'Content-Type': 'application/json',
-        Authorization: 'Bearer '+this.API_KEY
+        Authorization: 'Bearer '+this.API_KEY_READ
       });
-    return this.http.get<Movie>(this.baseUrl, {headers});
+    return this.http.get<Movie>(`${this.baseUrl}/popular`, {headers: headers});
   }
 }

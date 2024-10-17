d3a90be6e48537e2f278542369b0edc523f98c03
TaiFilipelli <taielfilipelli@gmail.com>
2024-10-17 17:45:38 -0300

    new 404 page, optimized png and added app-router to properly set routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { RouterLink } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
@@ -8,11 +8,11 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent, FontAwesomeModule],
+  imports: [RouterOutlet, RouterLink, NavbarComponent, FooterComponent, FontAwesomeModule],
   template:`
   <section>
     <app-navbar></app-navbar>
-    <app-home></app-home>
+    <router-outlet></router-outlet>
     <app-footer></app-footer>
   </section>`,
   styleUrl: './app.component.css'
--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+
+  error = faXmark;
 
 }

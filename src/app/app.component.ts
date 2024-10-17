3f5c24dbc0792e95561f34559c4840103242fc2b
TaiFilipelli <taielfilipelli@gmail.com>
2024-10-17 14:05:57 -0300

    first components, parts and more stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, FooterComponent],
+  template:`
+  <section>
+    <app-navbar></app-navbar>
+    <app-home></app-home>
+    <app-footer></app-footer>
+  </section>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {

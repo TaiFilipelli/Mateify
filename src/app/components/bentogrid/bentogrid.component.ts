2e6b81d42c05a27ad828f156361565b3e27476c5
TaiFilipelli <taielfilipelli@gmail.com>
2024-10-17 19:17:05 -0300

    new images and working on loading flow, as well as placeholder and defer on viewport charge.

--- src/app/components/bentogrid/bentogrid.component.ts
+++ src/app/components/bentogrid/bentogrid.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-bentogrid',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './bentogrid.component.html',
   styleUrl: './bentogrid.component.css'
 })
 export class BentogridComponent {
+  loader = faCircleNotch;
 
 }

af1589a3525249f31d311d100e4e2085baba117c
TaiFilipelli <taielfilipelli@gmail.com>
2024-10-17 17:28:52 -0300

    new modifications, animations and icons.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+
+    faArrow = faArrowDown;
 
 }
